549125dbcc6e38b3ef2b99e867a081d046b9d422
Kyle Kroger <kyle.s.kroger@gmail.com>
2021-10-10 00:49:58 -0500

    Finished Exercise 4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import BackgroundShading from './components/BackgroundShading';
 
 function App() {
   return (
     <>
+      <BackgroundShading />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/BackgroundShading/BackgroundShading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './BackgroundShading.module.css';
+
+const BackgroundShading = () => {
+  return (
+    <div className={styles.lowerShading}></div>
+  )
+}
+
+export default BackgroundShading;
